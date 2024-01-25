8af0d8ef8949d4457b99bef2b97fba3a99bdb942
Cary Phillips <cary@ilm.com>
2024-01-25 09:30:21 -0800

    test4
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include <Python.h>
-#include <iostream>
-
-static PyObject *hello(PyObject *self, PyObject *args)
-{
-    std::cout << "hello!" << std::endl;
-    Py_RETURN_NONE;
-}
 
 static PyMethodDef methods[] = {
-    //  {"hello", hello, METH_VARARGS},
   {NULL, NULL},
 };
 
